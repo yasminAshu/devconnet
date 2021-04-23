3fcd2353ad6b700130a32ea7a899bca837a1acfd
Yasmin Agwan <yasminagwan@Shamshers-MacBook-Pro.local>
2021-04-23 21:18:09 +0530

    initila commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('API running')
+})
+
+app.listen(PORT, () => console.log(`server started on ${PORT} `));
